865d5694a386392281006c6360351b22c003f783
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-10-19 17:39:36 +0600

    Project Update

--- src/Pages/Dashboard/BlogPost.jsx
+++ src/Pages/Dashboard/BlogPost.jsx
@@ -44,6 +44,8 @@ const BlogPost = () => {
 
 
 
+
+
     const handleEditorChange = (content, editor) => {
         console.log(content)
         setContent(content)
@@ -51,6 +53,14 @@ const BlogPost = () => {
 
     const handleSubmitContent = (e) => {
         e.preventDefault()
+
+        if (!blogTitle || !blogImage) {
+            return Swal.fire("Please provide blog title and thumbnail.");
+        }
+        if (!content) {
+            return Swal.fire("Please write your content.");
+        }
+
 
         const contentData = {
             blogTitle: blogTitle,
@@ -80,81 +90,83 @@ const BlogPost = () => {
 
     return (
         <div className='min-h-screen m-5'>
-            <div className='flex justify-between items-center my-5'>
-                <div className=''>
-                    <input onChange={(e) => setBlogTitle(e.target.value)} className='w-96 input input-bordered' placeholder='Blog Title' type="text" />
-                </div>
-                <div className='flex items-center gap-3'>
-                    {
-                        uploading ? (
-                            <RotatingLines
-                                visible={true}
-                                height="50"
-                                width="40"
-                                color="grey"
-                                strokeWidth="5"
-                                animationDuration="0.75"
-                                ariaLabel="rotating-lines-loading"
-                                wrapperStyle={{}}
-                                wrapperClass=""
-                            />
-                        ) : (
-                            <div>
-                                {
-                                    blogImage ? (
-                                        <img className='w-20 h-10' src={blogImage} alt="" />
-                                    ) : (
-                                        <FaImage className='text-4xl' />
-                                    )
-                                }
+            <div>
+                <div className='flex justify-between items-center my-5'>
+                    <div className=''>
+                        <input onChange={(e) => setBlogTitle(e.target.value)} className='w-96 input input-bordered' placeholder='Blog Title' type="text" />
+                    </div>
+                    <div className='flex items-center gap-3'>
+                        {
+                            uploading ? (
+                                <RotatingLines
+                                    visible={true}
+                                    height="50"
+                                    width="40"
+                                    color="grey"
+                                    strokeWidth="5"
+                                    animationDuration="0.75"
+                                    ariaLabel="rotating-lines-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                />
+                            ) : (
+                                <div>
+                                    {
+                                        blogImage ? (
+                                            <img className='w-20 h-10' src={blogImage} alt="" />
+                                        ) : (
+                                            <FaImage className='text-4xl' />
+                                        )
+                                    }
+                                </div>
+                            )
+                        }
+                        <div className='cursor-pointer flex' onClick={() => document.querySelector('input[type="file"]').click()}>
+                            <div className='border p-2'>
+                                <h1 className='text-xl flex items-center gap-2'>
+                                    <span>Blog Thumbnail</span>
+                                    <FaFileImport />
+                                </h1>
                             </div>
-                        )
-                    }
-                    <div className='cursor-pointer flex' onClick={() => document.querySelector('input[type="file"]').click()}>
-                        <div className='border p-2'>
-                            <h1 className='text-xl flex items-center gap-2'>
-                                <span>Blog Thumbnail</span>
-                                <FaFileImport />
-                            </h1>
+                            <input onChange={handleImageHosting} hidden type="file" className="file-input file-input-bordered w-full max-w-xs" />
                         </div>
-                        <input onChange={handleImageHosting} hidden type="file" className="file-input file-input-bordered w-full max-w-xs" />
                     </div>
                 </div>
-            </div>
-            <form onSubmit={handleSubmitContent}>
-                <Editor
-                    apiKey={import.meta.env.VITE_CONTENT_EDITOR_API_KEY}
-                    initialValue='<p>Hello, World</p>'
-                    init={{
-                        height: 600,
-                        plugins: [
-                            // Core editing features
-                            'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                            // Your account includes a free trial of TinyMCE premium features
-                            // Try the most popular premium features until Oct 29, 2024:
-                            'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
-                            //  add  text color plugins
-                            'textcolor', 'forecolor', 'backcolor'
-                        ],
-                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough forecolor backcolor | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                        forced_root_block: false, // Prevents auto <p> wrapping
-                        force_br_newlines: true,  // Ensures line break with <br>
-                        force_p_newlines: false,  // Disable <p> tag for new lines
-                        content_style: "body, p, h1, h2, h3, h4, h5, h6 { margin: 0; padding: 0; }",
-                        tinycomments_mode: 'embedded',
-                        tinycomments_author: 'Author name',
-                        mergetags_list: [
-                            { value: 'First.Name', title: 'First Name' },
-                            { value: 'Email', title: 'Email' },
-                        ],
-                        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-                    }}
+                <form className='h-[600px] border rounded-2xl' onSubmit={handleSubmitContent}>
+                    <Editor
+                        apiKey={import.meta.env.VITE_CONTENT_EDITOR_API_KEY}
+                        initialValue='<p>Hello, World</p>'
+                        init={{
+                            height: 600,
+                            plugins: [
+                                // Core editing features
+                                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                                // Your account includes a free trial of TinyMCE premium features
+                                // Try the most popular premium features until Oct 29, 2024:
+                                'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+                                //  add  text color plugins
+                                'textcolor', 'forecolor', 'backcolor'
+                            ],
+                            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough forecolor backcolor | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                            forced_root_block: false, // Prevents auto <p> wrapping
+                            force_br_newlines: true,  // Ensures line break with <br>
+                            force_p_newlines: false,  // Disable <p> tag for new lines
+                            content_style: "body, p, h1, h2, h3, h4, h5, h6 { margin: 0; padding: 0; }",
+                            tinycomments_mode: 'embedded',
+                            tinycomments_author: 'Author name',
+                            mergetags_list: [
+                                { value: 'First.Name', title: 'First Name' },
+                                { value: 'Email', title: 'Email' },
+                            ],
+                            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+                        }}
 
-                    onEditorChange={handleEditorChange}
-                />
-                <button type='submit' className='btn w-32 mt-5 bg-[#3bb77e] text-white'>Post</button>
-            </form>
-        </div>
+                        onEditorChange={handleEditorChange}
+                    />
+                    <button type='submit' className='btn w-32 mt-5 bg-[#3bb77e] text-white'>Post</button>
+                </form>
+            </div>
+        </div >
     );
 };
 
