37d2b4fa3f29093a904ee6cc7d3aadef833b81f0
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-11-08 23:43:07 +0600

    Project Update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,12 +5,16 @@ import { useForm } from 'react-hook-form';
 import useAuth from '../../Hooks/useAuth';
 import Swal from 'sweetalert2';
 import { Helmet } from 'react-helmet-async';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import { useState } from 'react';
 
 const Login = () => {
 
 
     const { loginSystem, googleAuthSystem } = useAuth()
     const navigate = useNavigate()
+    const axiosPublic = useAxiosPublic()
+    const [isRequired, setIsRequired] = useState("")
 
     const {
         register,
@@ -22,14 +26,38 @@ const Login = () => {
 
     const onSubmit = (data) => {
         console.log(data)
-        loginSystem(data.email, data.password)
+
+        const dataInfo = {
+            email: data.email,
+            password: data.password
+        }
+        axiosPublic.get('/login-matched', { params: dataInfo })
             .then(res => {
                 console.log(res.data)
-                navigate("/")
+
+                if (res.data.email === true && res.data.password === false) {
+                    return setIsRequired("Your password is incorrect")
+                }
+                if (data.password.length > 0) {
+                    setIsRequired("")
+                }
+
+                if (res.data.email && res.data.password) {
+                    loginSystem(data.email, data.password)
+                        .then(res => {
+                            console.log(res.data)
+                            navigate("/")
+                        })
+                        .catch(error => {
+                            console.log(error.message)
+                        })
+                }
             })
             .catch(error => {
                 console.log(error.message)
             })
+
+
 
     }
 
@@ -63,6 +91,7 @@ const Login = () => {
                         <div>
                             <input {...register("password", { required: true })} className={`input input-bordered w-full ${errors.email && 'border-red-500 focus:outline-red-500'}`} placeholder='Password' type="password" />
                             {errors.password && <span className='text-red-500'>This field is required</span>}
+                            <span className='text-red-500'>{isRequired}</span>
                         </div>
                         <div>
                             <button className='btn w-full bg-[#3bb77e] text-white' type='submit'>Login</button>
